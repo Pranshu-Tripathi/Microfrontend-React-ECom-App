e56eefec02027d0f0ccb6769354bb2ed1148737c
Pranshu Tripathi <pranshu.tripathi@schrodinger.com>
2024-09-30 01:56:04 +0530

    Added marketing production setup
    	- setted up the marketing.yml file
    	- Added invlaidation automation for remoteEntry.js file
    	- added the production domain env for files related to marketing.

--- linking-apps/marketing/config/webpack.prod.js
+++ linking-apps/marketing/config/webpack.prod.js
@@ -7,6 +7,7 @@ const prodConfig = {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
+        publicPath: '/linking-apps/marketing/latest/',
     },
     plugins: [
         new ModuleFederationPlugin({
